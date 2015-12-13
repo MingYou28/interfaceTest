bdf2d4524294eda471cc732ab1e4d0c1ea54540c
Ming You <mingyyou28@gmail.com>
2015-12-13 17:50:06 -0500

    Added rest of java files

---
+++ Prism.java
@@ -0,0 +1,19 @@
+public class Prism extends Rectangle implements Volume{
+    private double height;
+
+    public Prism(double length, double width, double height, String name){
+	super(length,width,name);
+	this.height = height;
+    }
+
+    public double getHeight(){
+	return height;
+    }
+
+    public String toString(){
+	return "Prism "+getName()+" that is "+getLength()+" by "+getWidth()+" by "+getHeight();
+    }
+    public double getVolume(){
+	return getLength() * getWidth() * getHeight();
+    }
+}
